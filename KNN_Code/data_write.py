75e83250015b29e6253a2ad2a69ffa2a087609cb
benicamera <66155472+benicamera@users.noreply.github.com>
2021-01-12 08:55:56 +0100

    API konform gemacht

--- KNN_Code/data_write.py
+++ KNN_Code/data_write.py
@@ -1,89 +1,92 @@
-# qSort programm 30.10.2020  -  Jeremias Boucsein
+# qSort programm 31.10.2020  -  Jeremias Boucsein
 #
-# Speichert die Daten für das KNN in einer Json Datei
+# Programm um das KNN einzulerenen und eine Vorhersage von ihm umzuformatieren
 #
 # How to use:
-#           Funktion data_write_positive:
-#                      Diese Funktion wird jedesmal wenn ein Objekt gekauft wurde aufgerufen.
-#                      Sie darf NICHT VOR der initialize_json_file Funktion aufgerufen werden.
-#                      Wenn ein Objekt vom Nutzer neu erstellt wurde,
-#                      sodass es NICHT in der initialize_json_file Funktion inizialisiert wurde,
-#                      kann es DENNOCH an die Funktion übergeben werden.
-#                      Die Funktion initialisiert das neue Objekt dann.
-#                      Als PARAMETER wird der Funktion ein String übergeben.
-#                      Dieser String ist der Name/die ID des Objekts, das gekauft wurde.
-#                      Die Funktion hat keine Rückgabe.
-#           Funktion data_write_negative:
-#                      Diese Funktion soll alle 25 Stunden einmal aufgerufen werden.
-#                      Sie hat keine Parameter und keine Rückgabe
+#           Funktion learn_and_predict:
+#                   Diese Funktion wirde jeden Morgen einmal aufgerufen, um die Vorschläge für den Tag zu errechnen.
+#                   Als PARAMETER wird ihr eine Liste von Strings übergeben. Die Strings sind die Items bzw.
+#                   deren ID.
+#                   Als Rückgabe wird eine Liste an Strings zurückgegeben. Die Strings sind auch hier die Items bzw.
+#                   deren ID. Die zurückgegebenen Items in der Liste sind die Items, die dem Nutzer der App
+#                   vorgeschlagen werden sollen. Dabei ist das erste Item in der Liste das, das am wahrscheinlichsten
+#                   gekauft wird.
 #
-import time
 import json
+
+from data_read import data_read_positive, data_read_negative, data_read_to_predict
+from bubble_sort_hussein import bubblesort
+import KNN
 from numpy import random
-from bubble_sort_hussein import get_time
+import numpy as np
 
 
-def data_write_positive(object):
-    with open('data.json') as json_file:
-        data = json.load(json_file)
+def get_training_data(object, _json):
+    training_data = []
+    pos_examples = data_read_positive(object, _json)
+    neg_examples = data_read_negative(object, _json)
 
-        if object not in data:      # Wenn das Objket noch nicht vorhanden ist, erstelle es
-            data[object] = {
-                'positive': [],
-                'negative': []
-            }
+    while len(neg_examples) > len(pos_examples):
+        del neg_examples[random.randint(0, len(neg_examples))]
 
-        temp = data[object]['positive']
+    training_data.extend(pos_examples)
+    training_data.extend(neg_examples)
 
-        y = get_time()  # Aufrufen des Datums relativ zum 1.1.2020
-
-        temp.append(y)  # Daten zu 'positive' hinzufügen
-
-        if len(temp) > 9:  # es braucht nur 9 Daten zu speichern, das reicht zum training
-            del temp[0]
-
-    with open('data.json', 'w') as f:      # Den Alten Datensatz durch den neuen überschreiben
-        json.dump(data, f, indent=4)
+    return training_data
 
 
-def data_write_negative():
-    do_it = False
+def learn_and_predict(objectStringJson):
+    data = json.loads(objectStringJson)
+    list_objects = data.keys()
+    prediction_output = []
+    item_list_output = []
+    for object in list_objects:
 
-    with open('data.json') as json_file:
-        data = json.load(json_file)
+        knn = KNN.MLP(eta=0.03, iterations=30000, n_input_neurons=4, n_hidden_neurons=3)
 
-        for obj in data:
-            if len(data[obj]['positive']) >= 4:     # Nur wenn des mindestens 4 Werte gibt
-                temp_list = data[obj]['positive'][-4:]  # Die letzten vier Daten aus dem Datenzatz nutzen
-                current_date = get_time()
+        p_data = data_read_to_predict(object)       # Daten zur Vorhersage
 
-                for h in range(len(temp_list)):     # Errechnen der Zeitdifferenz zu heute
-                    temp_list[h] = (temp_list[h] - current_date) / 10
+        if len(p_data) > 0:             # Wenn das Objekt das letzte mal vor länger als 40 Tagen gekauft wurde,
+            # ist der Return von data_read_to_predict() eine leere Liste. Dann soll das Netzt, um die Rechenzeit zu
+            # minmieren, nicht mit dem Datensatz trainiert werden. Es wird eine Prediction von 0 zurückgegeben.
+            t_data = get_training_data(object)      # Daten zum trainieren
+            if len(p_data) == 5 and len(t_data) == 10:      # Nur wenn alle Daten richtig formatiert sind
 
-                temp_list.insert(0, 1)              # Hinzufügen des Bias an den Anfang
+                knn.fit(t_data)                 # trainieren des KNN
 
-                if temp_list[-1] != 0:   # Wenn das Objekt heute nicht gekauft wurde,
-                    # kann das heutige Datum als Negativ-Beispiel verwendet werden
-                    do_it = True
+                pre = knn.predict(p_data)       # KNN errechnen die Vorhersage
 
-                    data[obj]['negative'].append(temp_list)
+                knn.plot()                      # erstellt einen Plot (zum Debuggen)
 
-                    if len(data[obj]['negative']) > 9:      # begrenzt Anzahl an Negativ-Beispielen
-                        random_number = random.randint(0, 9)    # Um keinen zu gleichmäßigen Datensatz zu erhalten
-                        del data[obj]['negative'][random_number]
+                prediction_output.append(pre)
+                item_list_output.append(object)
+        else:
+            prediction_output.append(np.array([0]))     # 0 als Array für die Einheitlichkeit
+            item_list_output.append(object)
 
-    if do_it:                   # nur wenn mindestens eine Änderung vogenommen wurde
-        with open('data.json', 'w') as json_file:
-            json.dump(data, json_file, indent=4)
+    print("P out", prediction_output)
+    print("Liste", item_list_output)
+
+    bubblesort(prediction_output, item_list_output)
+
+    print("P out", prediction_output)
+    print("Liste", item_list_output)
+
+    output = []
+    """for i, o in enumerate(item_list_output):       # Nur Items mit einer Kaufwahrscheinlichkeit von
+        # über 60 % vorschlagen. Und Umsortierne, sodass das wahrscheinlichste am Anfang ist
+        if prediction_output[i] > 0.6:
+            output.insert(0, o)"""
+    for i in range(-1, -1 * len(item_list_output), -1):
+        output.append(item_list_output[i])
+        if i == -12:
+            break
+
+    return output
 
 
 if __name__ == '__main__':
-    liste = ["apfel", "karotte", "birne", "tomate", "mango", "trauben", "salat", "brot", "toast", "marmelade", "schokolade", "kekse", "nudeln", "reis"]
-    # data_write_positive('tomate')
-    for i in range(11):
-        for o in liste:
-            data_write_positive(o)
-        r = random.randint(1, 4)  # in sekunden
-        time.sleep(r)
-        data_write_negative()
-        time.sleep(5 - r)
+    objekte = ["apfel", "karotte", "birne", "tomate", "mango", "trauben", "salat", "brot", "toast", "marmelade", "schokolade", "kekse", "nudeln", "reis"]
+
+    p = learn_and_predict(objekte)
+    print(p)
